f2fae9d8b61263e3b4f2cd72db7c76fd04170837
baronix <baronwork@icloud.com>
2025-04-01 10:51:01 +0100

    commit inicial

--- README.md
+++ README.md
@@ -3,3 +3,21 @@
 Este trabalho é realizado pelos alunos da Universidade Aberta do curso de Licenciatura em Engenharia Informática.
 Este trabalho é destinado apenas para fins académicos que estão incluidos na Unidade Curricular de "Laboratório de Desenvolvimento de Software". Como tal conta como elemento de avaliação curricular.
 
+# Estrutura do projeto
+Criado através do boilerplate "MVC" de "ASP.NET Core Web Application" do Visual Studio Community para garantir a correta estrutura do modelo arquitetónico Model View Controller.
+Foi implementada a Api JSON.NET através do pacote "Newtonsoft.Json" dos pacotes NuGet do VS.
+
+## Model
+TodoItem.cs dentro da pasta Models
+
+## Controller
+TodoController.cs dentro da pasta Controllers
+
+## View
+Index.cshtml dentro da pasta Views/Todo
+
+# API JSON.NET
+A api escolhida para o efeito guarda os dados Todo dentro da pasta App_Data no ficheiro todos.json
+
+# Como executar o programa?
+Após clonar o repositório, basta clicar em "Play" ou F5 no modo "HTTP"
---
+++ ToDoList/Controllers/TodoController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly string _dataFilePath = "App_Data/todos.json";
+
+        // Mostra a lista de tarefas
+        public IActionResult Index()
+        {
+            var todos = GetAllTodos();
+            return View(todos);
+        }
+
+        // Adiciona nova tarefa
+        [HttpPost]
+        public IActionResult Create(TodoItem todo)
+        {
+            var todos = GetAllTodos();
+
+            todo.Id = todos.Count > 0 ? todos.Max(t => t.Id) + 1 : 1;
+            todo.CreatedAt = DateTime.Now;
+
+            todos.Add(todo);
+            SaveTodos(todos);
+
+            return RedirectToAction("Index");
+        }
+
+        // Marca/desmarca tarefa como completa
+        [HttpPost]
+        public IActionResult ToggleComplete(int id)
+        {
+            var todos = GetAllTodos();
+            var todo = todos.Find(t => t.Id == id);
+
+            if (todo != null)
+            {
+                todo.IsComplete = !todo.IsComplete;
+                SaveTodos(todos);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        // Remove uma tarefa
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var todos = GetAllTodos();
+            var todo = todos.Find(t => t.Id == id);
+
+            if (todo != null)
+            {
+                todos.Remove(todo);
+                SaveTodos(todos);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        // Lê todas as tarefas do arquivo JSON
+        private List<TodoItem> GetAllTodos()
+        {
+            EnsureDirectoryExists();
+
+            if (System.IO.File.Exists(_dataFilePath))
+            {
+                var json = System.IO.File.ReadAllText(_dataFilePath);
+                return JsonConvert.DeserializeObject<List<TodoItem>>(json) ?? new List<TodoItem>();
+            }
+
+            return new List<TodoItem>();
+        }
+
+        // Salva as tarefas no arquivo JSON
+        private void SaveTodos(List<TodoItem> todos)
+        {
+            EnsureDirectoryExists();
+
+            var json = JsonConvert.SerializeObject(todos, Formatting.Indented);
+            System.IO.File.WriteAllText(_dataFilePath, json);
+        }
+
+        // Garante que o diretório App_Data existe
+        private void EnsureDirectoryExists()
+        {
+            var directory = Path.GetDirectoryName(_dataFilePath);
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+        }
+    }
+}
