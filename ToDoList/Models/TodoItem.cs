572e9ba6725cf61518f0c71e1ca6dc04faedd7c6
mbaltar13 <47746362+mbaltar13@users.noreply.github.com>
2025-04-21 10:58:20 +0100

    Update TodoItem.cs
    Correção do titulo, passa a ser obrigatorio

--- ToDoList/Models/TodoItem.cs
+++ ToDoList/Models/TodoItem.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 
 namespace ToDoList.Models
 {
     public class TodoItem
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        public required string Title { get; set; } // Verificador::Acrescentei required
         public bool IsComplete { get; set; }
         public DateTime CreatedAt { get; set; }
     }
