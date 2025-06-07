75392c39c651a6e64ab6e5cbcf5956ecb1904be7
José Manuel <120184614+905525@users.noreply.github.com>
2025-06-07 21:47:04 +0100

    Update ErrorViewModel.cs

--- ToDoList/Models/ErrorViewModel.cs
+++ ToDoList/Models/ErrorViewModel.cs
@@ -1,6 +1,6 @@
 namespace ToDoList.Models
 {
-    // Classe Responsavel pela verificação e validação tarefa inserida
+    // Classe Responsavel pela verificação e validação da tarefa inserida
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
